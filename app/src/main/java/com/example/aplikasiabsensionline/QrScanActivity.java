7ba42e455cf08605e74e273bb9e97c65d1175b57
Rizki Noviyandi <rizkinoviyandi2001@gmail.com>
2024-03-17 21:39:32 +0700

    update

---
+++ app/src/main/java/com/example/aplikasiabsensionline/QrScanActivity.java
@@ -0,0 +1,14 @@
+package com.example.aplikasiabsensionline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QrScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scan);
+    }
+}
---
+++ app/src/main/java/com/example/aplikasiabsensionline/RecyclerAdapter.java
@@ -0,0 +1,2 @@
+package com.example.aplikasiabsensionline;public class RecyclerAdapter {
+}
